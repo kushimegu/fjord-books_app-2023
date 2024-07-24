986080b4185e7e77f2f17f628a9337d6dd84c97a
Kushizaki <kushimegu288@gmail.com>
2024-07-24 16:43:40 +0900

    ユーザー名の登録を必須に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :password, presence: true, on: :create
+  validates :name, presence: true, on: :create
   validates :bio, length: { maximum: 200 }
   attr_accessor :current_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_23_080557) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_24_064149) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_23_080557) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.string "postal_code"
     t.string "address"
     t.text "bio"
