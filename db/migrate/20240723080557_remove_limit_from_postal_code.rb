4b60cf46146f003ed84fb569e665fe9c2d083688
Kushizaki <kushimegu288@gmail.com>
2024-07-30 14:22:03 +0900

    マイグレーションファイルの統合

--- db/migrate/20240723080557_remove_limit_from_postal_code.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLimitFromPostalCode < ActiveRecord::Migration[7.0]
-  def change
-    change_column :users, :postal_code, :string, limit:nil
-  end
-end
--- db/migrate/20240724064149_add_not_null_to_name.rb
+++
@@ -1,5 +0,0 @@
-class AddNotNullToName < ActiveRecord::Migration[7.0]
-  def change
-    change_column :users, :name, :string, null: false
-  end
-end
