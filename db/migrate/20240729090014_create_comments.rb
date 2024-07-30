2b53558b3e2006676075c6c5451c26d7f47b5574
Kushizaki <kushimegu288@gmail.com>
2024-07-30 16:20:21 +0900

    マイグレーションファイルの統合

--- db/migrate/20240729090014_create_comments.rb
+++ db/migrate/20240729090014_create_comments.rb
@@ -2,6 +2,8 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :content
+      t.references :commentable, polymorphic: true, null: false
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
