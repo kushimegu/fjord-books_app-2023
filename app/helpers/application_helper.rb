476d960eb7471c69a13a7ae50001469362cfbfaa
Kushizaki <kushimegu288@gmail.com>
2024-08-06 17:56:38 +0900

    メンションリンクを別タブで開かないように修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,12 +19,11 @@ module ApplicationHelper
     safe_join(content.split("\n"), tag.br)
   end
 
-  def text_url_to_link(text)
-    require 'uri'
+  def text_url_to_links(text)
     URI.extract(text, ['http', 'https']).uniq.each do |url|
-      sub_text = "<a href=#{url} target=\'_blank\'>#{url}</a>"
-      text.gsub!(url, sub_text)
-    return text
+      link = "<a href=#{url}>#{url}</a>"
+      text.gsub!(url, link)
     end
+    text
   end
 end
