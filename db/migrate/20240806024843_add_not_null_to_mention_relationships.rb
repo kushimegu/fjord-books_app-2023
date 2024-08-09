2c04aea8ec0fd3c75a4b78884972091dfb9f0630
Kushizaki <kushimegu288@gmail.com>
2024-08-09 17:19:15 +0900

    クラス名の修正

--- db/migrate/20240806024843_add_not_null_to_mention_relationships.rb
+++ db/migrate/20240806024843_add_not_null_to_mention_relationships.rb
@@ -1,4 +1,4 @@
-class Add NotNullToMentionRelationships < ActiveRecord::Migration[7.0]
+class AddNotNullToMentionRelationships < ActiveRecord::Migration[7.0]
   def change
     change_column_null :mention_relationships, :mentioning_id, false
     change_column_null :mention_relationships, :mentioned_id, false
