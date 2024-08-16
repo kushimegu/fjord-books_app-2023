73ade1b6d6d8bc14ed44e69eecd85e7c8daaef58
Kushizaki <kushimegu288@gmail.com>
2024-08-16 11:27:18 +0900

    マイグレーションファイルの統合

--- db/migrate/20240802060617_remove_null_false_from_user_in_comments.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNullFalseFromUserInComments < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :comments, :user_id, true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_02_060617) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_29_090014) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_02_060617) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -50,11 +50,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_02_060617) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "commentable_type", null: false
     t.integer "commentable_id", null: false
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -62,9 +62,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_02_060617) do
   create_table "reports", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
