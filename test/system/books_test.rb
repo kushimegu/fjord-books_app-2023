ea84eb43a78c4c5d6bbe52e7677347d0ff1a3b0a
Kushizaki <kushimegu288@gmail.com>
2024-08-30 15:16:20 +0900

    booksのシステムテストが通るように変更

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,42 +4,56 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    @book = books(:rails)
+
+    visit root_url
+    assert_css 'h2', text: 'ログイン'
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+    assert_text 'ログインしました。'
   end
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: 'Books'
+
+    assert_selector 'h1', text: '本の一覧'
   end
 
   test 'should create book' do
     visit books_url
-    click_on 'New book'
+    click_on '本の新規作成'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in 'タイトル', with: 'Ruby超入門'
+    fill_in 'メモ', with: 'わかりやすい'
+    fill_in '著者', with: '五十嵐邦明'
+    click_on '登録する'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text '本が作成されました。'
+    assert_text 'Ruby超入門'
+    assert_text 'わかりやすい'
+    assert_text '五十嵐'
   end
 
   test 'should update Book' do
     visit book_url(@book)
-    click_on 'Edit this book', match: :first
+    click_on 'この本を編集', match: :first
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in 'タイトル', with: 'Ruby入門'
+    fill_in 'メモ', with: '名著です'
+    fill_in '著者', with: '伊藤淳一'
+    click_on '更新する'
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text '本が更新されました。'
+    assert_text 'Ruby入門'
+    assert_text '名著です'
+    assert_text '伊藤淳一'
   end
 
   test 'should destroy Book' do
     visit book_url(@book)
-    click_on 'Destroy this book', match: :first
+    click_on '削除', match: :first
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました。'
   end
 end
