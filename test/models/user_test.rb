3ff3238c70a004f227c30f3402b832c14b7da768
Kushizaki <kushimegu288@gmail.com>
2024-09-09 13:09:19 +0900

    userモデルテストのサンプルデータをリアルに変更

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,11 +4,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'name_or_email' do
-    user = User.new(email: 'foo@example.com', name: '')
+    user = User.new(email: 'yamada@example.com', name: '')
 
-    assert_equal 'foo@example.com', user.name_or_email
+    assert_equal 'yamada@example.com', user.name_or_email
 
-    user.name = 'Foo Bar'
-    assert_equal 'Foo Bar', user.name_or_email
+    user.name = '山田太郎'
+    assert_equal '山田太郎', user.name_or_email
   end
 end
